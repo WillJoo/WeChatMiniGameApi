22aa540c56a2be526a2ee8190603f68b3b84844b
Will <zhuqw@outlook.com>
2019-02-15 10:29:44 +0800

    整理子域定义文件,可在子域单独使用

--- wxo.d.ts
+++ wxo.d.ts
@@ -6,6 +6,131 @@
  * Reorganized by: Will Zhu <https://github.com/WillJoo>
  */
 
+//声明全局变量
+declare const canvas: wx.types.Canvas;
+
+//声明全局函数
+
+/**
+ * 在下次进行重绘时执行
+ * @param callback
+ * @returns 请求 ID
+ */
+declare function requestAnimationFrame(callback: () => void): number;
+
+/**
+ * 取消一个先前通过调用 @function requestAnimationFrame 方法添加到计划中的动画帧请求
+ * @param requestID 请求 ID
+ */
+declare function cancelAnimationFrame(requestID: number): void;
+
+/**
+ * 设定一个定时器。在定时到期以后执行注册的回调函数
+ * @param callback 回调函数
+ * @param delay 延迟的时间，函数的调用会在该延迟之后发生，单位 ms。
+ * @param rest param1, param2, ..., paramN 等附加参数，它们会作为参数传递给回调函数。
+ * @return 定时器的编号。这个值可以传递给 clearTimeout 来取消该定时。
+ */
+declare function setTimeout(callback: () => void, delay: number, ...rest: any[]): number;
+/**
+ * 取消由 setTimeout 设置的定时器。
+ * @param timeoutID 要取消的定时器的 ID
+ */
+declare function clearTimeout(timeoutID: number): void;
+/**
+ * 设定一个定时器。按照指定的周期（以毫秒计）来执行注册的回调函数
+ * @param callback 回调函数
+ * @param delay 执行回调函数之间的时间间隔，单位 ms。
+ * @param rest param1, param2, ..., paramN 等附加参数，它们会作为参数传递给回调函数。
+ * @return 定时器的编号。这个值可以传递给 clearInterval 来取消该定时。
+ */
+declare function setInterval(callback: () => void, delay: number, ...rest: any[]): number;
+/**
+ * 取消由 setInterval 设置的定时器。
+ * @param intervalID 要取消的定时器的 ID
+ */
+declare function clearInterval(intervalID: number): void;
+
+declare namespace wx {
+    namespace types {
+        interface Touch {
+            /**
+             * Touch 对象的唯一标识符，只读属性
+             * 一次触摸动作(我们值的是手指的触摸)在平面上移动的整个过程中, 该标识符不变。
+             * 可以根据它来判断跟踪的是否是同一次触摸过程。
+             */
+            identifier: number;
+            /**
+             * 触点相对于屏幕左边沿的 X 坐标。
+             */
+            screenX: number;
+            /**
+             * 触点相对于屏幕左边沿的 Y 坐标。
+             */
+            screenY: number;
+        }
+        interface TouchEvent {
+            /**
+             * 当前所有触摸点的列表
+             */
+            touches: Touch[];
+            /**
+             * 触发此次事件的触摸点列表
+             */
+            changedTouches: Touch[];
+            /**
+             * 事件触发时的时间戳
+             */
+            timeStamp: number;
+        }
+    }
+    /**
+     * 监听开始始触摸事件
+     */
+    function onTouchStart(callback: (res: types.TouchEvent) => void): void;
+    function offTouchStart(callback: (res: types.TouchEvent) => void): void;
+    /**
+     * 监听触点移动事件
+     */
+    function onTouchMove(callback: (res: types.TouchEvent) => void): void;
+    function offTouchMove(callback: (res: types.TouchEvent) => void): void;
+    /**
+     * 监听触摸结束事件
+     */
+    function onTouchEnd(callback: (res: types.TouchEvent) => void): void;
+    function offTouchEnd(callback: (res: types.TouchEvent) => void): void;
+    /**
+     * 监听触点失效事件
+     */
+    function onTouchCancel(callback: (res: types.TouchEvent) => void): void;
+    function offTouchCancel(callback: (res: types.TouchEvent) => void): void;
+}
+
+declare namespace wx {
+    namespace types {
+        interface Response {
+            /* 错误信息 */
+            errMsg: string;
+            /* 错误码 */
+            errCode?: number;
+        }
+        interface CallbackParams<T extends Response = Response> {
+            /**
+             * 接口调用成功的回调函数
+             */
+            success?: (res: T) => void;
+            /**
+             * 接口调用失败的回调函数
+             */
+            fail?: (res: Response) => void;
+            /**
+             * 接口调用结束的回调函数（调用成功、失败都会执行）
+             */
+            complete?: (res: T) => void;
+        }
+    }
+}
+
 declare namespace wx {
     namespace types {
         interface ToTempFileSyncParams {
@@ -125,7 +250,92 @@ declare namespace wx {
              */
             toDataURL(): string;
         }
-        interface OpenContextUserInfoParam extends CallbackParams<OpenContextUserInfoResponse> {
+    }
+    /**
+     * 创建一个画布对象。首次调用创建的是显示在屏幕上的画布，之后调用创建的都是离屏画布。
+     * @returns 画布对象
+     */
+    function createCanvas(): types.Canvas;
+    /**
+     * 获取主域和开放数据域共享的 sharedCanvas
+     * 该接口只可在开放数据域下使用。
+     */
+    function getSharedCanvas(): types.Canvas;
+}
+
+declare namespace wx {
+    namespace types {
+        interface Image extends HTMLImageElement {
+            /**
+             * 图片的 URL
+             */
+            src: string;
+            /**
+             * 图片的真实宽度
+             */
+            width: number;
+            /**
+             * 图片的真实高度
+             */
+            height: number;
+            /**
+             * 图片加载完成后触发的回调函数
+             */
+            onload(): void;
+            /**
+             * 图片加载发生错误后触发的回调函数
+             */
+            onerror(): void;
+        }
+    }
+    /**
+     * 创建一个图片对象
+     * @returns 图片对象
+     */
+    function createImage(): types.Image;
+}
+
+declare namespace wx {
+    namespace types {
+        interface UserInfo {
+            /**
+             * 用户 openId
+             * 只在子域可用
+             */
+            openId?: string;
+            /**
+             * 用户昵称
+             */
+            nickName: string;
+            /**
+             * 用户头像图片 url
+             */
+            avatarUrl: string;
+            /**
+             * 用户性别
+             * @argument 0 未知
+             * @argument 1 男
+             * @argument 2 女
+             */
+            gender: 0 | 1 | 2;
+            /**
+             * 用户所在国家
+             */
+            country: string;
+            /**
+             * 用户所在省份
+             */
+            province: string;
+            /**
+             * 用户所在城市
+             */
+            city: string;
+            /**
+             * 显示 country province city 所用的语言
+             */
+            language: string;
+        }
+        interface GetUserInfoParams extends CallbackParams<GetUserInfoResponse> {
             /**
              * 要获取信息的用户的 openId 数组，如果要获取当前用户信息，则将数组中的一个元素设为 'selfOpenId'
              */
@@ -135,12 +345,21 @@ declare namespace wx {
              */
             lang?: "en" | "zh_CN" | "zh_TW";
         }
-        interface OpenContextUserInfoResponse extends Response {
+        interface GetUserInfoResponse extends Response {
             /**
              * 用户信息列表
              */
             data: UserInfo[];
         }
+    }
+    /**
+     * 在无须用户授权的情况下，批量获取用户信息。该接口只在开放数据域下可用
+     */
+    function getUserInfo(params: types.GetUserInfoParams): void;
+}
+
+declare namespace wx {
+    namespace types {
         interface KVData {
             /**
              * 数据的 key
@@ -204,15 +423,6 @@ declare namespace wx {
         }
     }
     /**
-     * 获取主域和开放数据域共享的 sharedCanvas
-     * 该接口只可在开放数据域下使用。
-     */
-    function getSharedCanvas(): types.Canvas;
-    /**
-     * 在无须用户授权的情况下，批量获取用户信息。该接口只在开放数据域下可用
-     */
-    function getUserInfo(params: types.OpenContextUserInfoParam): void;
-    /**
      * 拉取当前用户所有同玩好友的托管数据。
      * 该接口只可在开放数据域下使用
      * 支持版本 >= 1.9.92
@@ -244,6 +454,9 @@ declare namespace wx {
      * 支持版本 >= 1.9.92
      */
     function setUserCloudStorage(params: types.SetUserCloudStorageParams): void;
+}
+
+declare namespace wx {
     /**
      * 监听主域发送的消息
      */
